bbdf54517a2ae05f87b2726610c3d639582b26e6
loiclau <loic.lau@gmail.com>
2018-02-16 14:54:40 +0100

    feat(export) add loading

--- www/modules/centreon-awie/core/js/Export.js
+++ www/modules/centreon-awie/core/js/Export.js
@@ -9,6 +9,7 @@ function selectFilter(selected) {
 
 function submitForm() {
     var data = jQuery("#exportForm").serializeArray();
+    jQuery(".loadingWrapper").css('display', 'block');
     jQuery.ajax({
         type: "POST",
         url: "./modules/centreon-awie/core/generateExport.php",
@@ -24,6 +25,7 @@ function submitForm() {
                 alert(errorMsg);
             }
             jQuery("#downloadForm").submit();
+            jQuery(".loadingWrapper").css('display', 'none');
         },
     });
     event.preventDefault();
